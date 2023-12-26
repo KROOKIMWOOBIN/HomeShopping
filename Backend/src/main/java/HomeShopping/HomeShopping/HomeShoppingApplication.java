ce366fe648d14565e38fbc16c1cde59e0f6dc34a
KIMWOOBIN <woodol4540@naver.com>
2023-12-26 15:54:36 +0900

    Day1226

---
+++ Backend/src/main/java/HomeShopping/HomeShopping/HomeShoppingApplication.java
@@ -0,0 +1,13 @@
+package HomeShopping.HomeShopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeShoppingApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/HomeShopping/HomeShopping/HomeShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package HomeShopping.HomeShopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
