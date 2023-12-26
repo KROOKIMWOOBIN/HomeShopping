0c617fd46a17d94756d20fd949bee5bcc2fe5235
kr-younghoon <sullanta0802@gmail.com>
2023-12-27 03:32:54 +0900

    chore: 리액트 보일러플레이트 삭제 및 수정

--- FrontEnd/homeshopping/src/App.js
+++ FrontEnd/homeshopping/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './Components/LoginPage';
+import SignupPage from './Components/SignupPage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Router basename="/">
+                <Routes>
+                    <Route path="/" element={<LoginPage />} />
+                    <Route path="/signup" element={<SignupPage />} />
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
